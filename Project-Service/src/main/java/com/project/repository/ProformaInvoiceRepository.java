e893745a4d57d1262da07d96eac17ff6b0d779b2
Kartik Nikhade <kartiknikhade077@gmail.com>
2025-08-21 14:59:04 +0530

    commit unwanted changes

--- Project-Service/src/main/java/com/project/repository/ProformaInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entity.ProformaInvoice;
-
-public interface ProformaInvoiceRepository extends JpaRepository<ProformaInvoice, String> {
-
-}
--- Project-Service/src/main/java/com/project/repository/ProformaItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entity.ProformaItems;
-
-public interface ProformaItemsRepository extends JpaRepository<ProformaItems, String> {
-
-}
